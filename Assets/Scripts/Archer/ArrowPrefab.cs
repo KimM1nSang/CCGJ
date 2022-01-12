11435bf18a9134294fc8fcd0e572a682b8c9959b
DESKTOP-MG9TML9\USER <datanet231@gmail.com>
2022-01-12 10:26:42 +0900

    궁수 화살 발사 구현(풀링 X)

---
+++ Assets/Scripts/Archer/ArrowPrefab.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowPrefab : MonoBehaviour
+{
+    void Update()
+    {
+        this.transform.right = GetComponent<Rigidbody2D>().velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // 몬스터 데미지 처리
+
+        Destroy(this.gameObject); // 나중에 풀링
+    }
+}
---
+++ Assets/Scripts/Archer/ShotArrow.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotArrow : MonoBehaviour
+{
+    const float shootingDelay = 2.5f;
+
+    private GameObject arrowPrefab;
+
+    private void Awake()
+    {
+        arrowPrefab = Resources.Load<GameObject>("Prefabs/Game/Archer/Arrow");
+    }
+
+    private void Start() // 나중에 게임 시작으로 바꾸기
+    {
+        StartCoroutine(ShootingArrow(shootingDelay));
+    }
+
+    IEnumerator ShootingArrow(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+
+            // 화살 발사
+            GameObject go = Instantiate(arrowPrefab, this.transform.position, Quaternion.Euler(0.0f, 0.0f, 50.0f));
+            go.GetComponent<Rigidbody2D>().velocity = go.transform.right * 8;
+        }
+    }
+}
